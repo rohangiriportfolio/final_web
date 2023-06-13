d4eb91e2293e2f7d8c14f525b6d7ba9d9c65885e
rohangiri00000 <132122182+rohangiri00000@users.noreply.github.com>
2023-06-13 21:59:26 +0530

    Add files via upload
    Final Web

---
+++ snake.js
@@ -0,0 +1,117 @@
+var blockSize = 15;
+var total_row = 25;
+var total_col = 25;
+var board;
+var context;
+var snakeX = blockSize * 12;
+var snakeY = blockSize * 12;
+var speedX = 0;
+var speedY = 0;
+var snakeBody = [];
+var foodX;
+var foodY;
+var gameOver = false;
+var score = 0;
+
+window.onload = function () {
+    board = document.getElementById("field");
+    board.height = total_row * blockSize;
+    board.width = total_col * blockSize;
+    context = board.getContext("2d");
+    placeFood();
+    document.addEventListener("keyup", changeDirection);
+    setInterval(update, 300);
+}
+function update() {
+    if (gameOver) {
+        speedX = 0;
+        speedY = 0;
+        return;
+    }
+    var grad = context.createLinearGradient(0, 375, 375, 0);
+    grad.addColorStop(0, 'rgb(22, 22, 255)');
+    grad.addColorStop(1, 'rgb(255, 52, 160)');
+    context.fillStyle = grad;
+    context.fillRect(0, 0, board.width, board.height);
+    context.fillStyle = "red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+    context.strokestyle = 'darkgreen';
+    context.strokeRect(foodX, foodY, blockSize, blockSize);
+    if (snakeX == foodX && snakeY == foodY) {
+        snakeBody.push([foodX, foodY]);
+        placeFood();
+        score++;
+        document.getElementById('score').innerHTML = "Score: " + score;
+    }
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+    if (snakeBody.length) {
+        snakeBody[0] = [snakeX, snakeY];
+    }
+    context.fillStyle = "rgb(172, 0, 172)";
+    snakeX += speedX * blockSize;
+    snakeY += speedY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+    context.strokestyle = 'darkgreen';
+    context.strokeRect(snakeX, snakeY, blockSize, blockSize);
+    for (let i = 0; i < snakeBody.length; i++) {
+        context.fillStyle = "violet";
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+        context.strokestyle = 'darkgreen';
+        context.strokeRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+    if (snakeX < 0 || snakeX >= total_col * blockSize || snakeY < 0 || snakeY >= total_row * blockSize) {
+        gameOver = true;
+        document.getElementById('main').innerHTML = "";
+        document.getElementById('main').innerHTML = "Your Score: " + score;
+        document.getElementById('main').style.fontSize = "30px";
+        document.getElementById('main').style.margin = "300px auto";
+        document.getElementById('main').style.color = "rgb(245, 0, 212)";
+        document.getElementById('main').style.border = "2px solid blue";
+        document.getElementById('main').style.width = "180px";
+        document.getElementById('main').style.display = "flex";
+        document.getElementById('main').style.justifyContent = "center";
+        document.getElementById('main').style.alignItems = "center";
+
+    }
+    for (let i = 0; i < snakeBody.length; i++) {
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+            gameOver = true;
+            document.getElementById('main').innerHTML = "";
+            document.getElementById('main').innerHTML = "Your Score: " + score;
+            document.getElementById('main').style.fontSize = "30px";
+            document.getElementById('main').style.margin = "300px auto";
+            document.getElementById('main').style.color = "rgb(245, 0, 212)";
+            document.getElementById('main').style.border = "2px solid blue";
+            document.getElementById('main').style.width = "180px";
+            document.getElementById('main').style.display = "flex";
+            document.getElementById('main').style.justifyContent = "center";
+            document.getElementById('main').style.alignItems = "center";
+        }
+    }
+}
+
+function changeDirection(e) {
+    if (e.code == "ArrowUp" && speedY != 1) {
+        speedX = 0;
+        speedY = -1;
+    }
+    else if (e.code == "ArrowDown" && speedY != -1) {
+        speedX = 0;
+        speedY = 1;
+    }
+    else if (e.code == "ArrowLeft" && speedX != 1) {
+        speedX = -1;
+        speedY = 0;
+    }
+    else if (e.code == "ArrowRight" && speedX != -1) {
+        speedX = 1;
+        speedY = 0;
+    }
+}
+
+function placeFood() {
+    foodX = Math.floor(Math.random() * total_col) * blockSize;
+    foodY = Math.floor(Math.random() * total_row) * blockSize;
+}
---
+++ tic_tac_toe.js
@@ -0,0 +1,351 @@
+var times = 0;
+let square = [0, 0, 0, 0, 0, 0, 0, 0, 0];//x=1,o=2
+function display1() {
+    if (times % 2 == 0) {
+        document.getElementById('turn1').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn1').value = 'X';
+        square[0] = 2;
+    }
+    else if (times % 2 != 0) {
+        document.getElementById('turn2').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn1').value = 'O';
+        square[0] = 1;
+    }
+    times++;
+    if (check() == 1) {
+
+    }
+    else if (times == 9) {
+        setTimeout(draw, 1000);
+    }
+}
+function display2() {
+    if (times % 2 == 0) {
+        document.getElementById('turn1').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn2').value = 'X';
+        square[1] = 2;
+    }
+    else if (times % 2 != 0) {
+        document.getElementById('turn2').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn2').value = 'O';
+        square[1] = 1;
+    }
+    times++;
+    if (check() == 1) {
+
+    }
+    else if (times == 9) {
+        setTimeout(draw, 1000);
+    }
+}
+function display3() {
+    if (times % 2 == 0) {
+        document.getElementById('turn1').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn3').value = 'X';
+        square[2] = 2;
+
+    }
+    else if (times % 2 != 0) {
+        document.getElementById('turn2').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn3').value = 'O';
+        square[2] = 1;
+    }
+    times++;
+    if (check() == 1) {
+
+    }
+    else if (times == 9) {
+        setTimeout(draw, 1000);
+    }
+}
+function display4() {
+    if (times % 2 == 0) {
+        document.getElementById('turn1').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn4').value = 'X';
+        square[3] = 2;
+    }
+    else if (times % 2 != 0) {
+        document.getElementById('turn2').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn4').value = 'O';
+        square[3] = 1;
+    }
+    times++;
+    if (check() == 1) {
+
+    }
+    else if (times == 9) {
+        setTimeout(draw, 1000);
+    }
+}
+function display5() {
+    if (times % 2 == 0) {
+        document.getElementById('turn1').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn5').value = 'X';
+        square[4] = 2;
+    }
+    else if (times % 2 != 0) {
+        document.getElementById('turn2').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn5').value = 'O';
+        square[4] = 1;
+    }
+    times++;
+    if (check() == 1) {
+
+    }
+    else if (times == 9) {
+        setTimeout(draw, 1000);
+    }
+}
+function display6() {
+    if (times % 2 == 0) {
+        document.getElementById('turn1').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn6').value = 'X';
+        square[5] = 2;
+    }
+    else if (times % 2 != 0) {
+        document.getElementById('turn2').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn6').value = 'O';
+        square[5] = 1;
+    }
+    times++;
+    if (check() == 1) {
+
+    }
+    else if (times == 9) {
+        setTimeout(draw, 1000);
+    }
+}
+function display7() {
+    if (times % 2 == 0) {
+        document.getElementById('turn1').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn7').value = 'X';
+        square[6] = 2;
+    }
+    else if (times % 2 != 0) {
+        document.getElementById('turn2').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn7').value = 'O';
+        square[6] = 1;
+    }
+    times++;
+    if (check() == 1) {
+
+    }
+    else if (times == 9) {
+        setTimeout(draw, 1000);
+    }
+}
+function display8() {
+    if (times % 2 == 0) {
+        document.getElementById('turn1').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn8').value = 'X';
+        square[7] = 2;
+    }
+    else if (times % 2 != 0) {
+        document.getElementById('turn2').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn8').value = 'O';
+        square[7] = 1;
+    }
+    times++;
+    if (check() == 1) {
+
+    }
+    else if (times == 9) {
+        setTimeout(draw, 1000);
+    }
+}
+function display9() {
+    if (times % 2 == 0) {
+        document.getElementById('turn1').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn9').value = 'X';
+        square[8] = 2;
+    }
+    else if (times % 2 != 0) {
+        document.getElementById('turn2').style = 'border-bottom: 2px solid cyan';
+        document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+        document.getElementById('btn9').value = 'O';
+        square[8] = 1;
+    }
+    times++;
+    if (check() == 1) {
+
+    }
+    else if (times == 9) {
+        setTimeout(draw, 1000);
+    }
+}
+function check() {
+    if ((square[0] == square[1]) && (square[1] == square[2]) && ((square[0] == 1) || (square[0] == 2))) {
+        document.getElementById('btn1').style.animation = "top_left_to_top_middle 1s linear";
+        document.getElementById('btn3').style.animation = "top_right_to_top_middle 1s linear";
+        screen_lock();
+        if (times % 2 != 0) {
+            document.getElementById('turn2').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_X, 1000);
+        }
+        else {
+            document.getElementById('turn1').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_O, 1000);
+        }
+        return 1;
+    }
+    else if ((square[3] == square[4]) && (square[4] == square[5]) && ((square[3] == 1) || (square[3] == 2))) {
+        document.getElementById('btn4').style.animation = "left_middle_to_middle 1s linear";
+        document.getElementById('btn6').style.animation = "right_middle_to_middle 1s linear";
+        screen_lock();
+        if (times % 2 != 0) {
+            document.getElementById('turn2').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_X, 1000);
+        }
+        else {
+            document.getElementById('turn1').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_O, 1000);
+        }
+        return 1;
+    }
+    else if ((square[6] == square[7]) && (square[7] == square[8]) && ((square[6] == 1) || (square[6] == 2))) {
+        document.getElementById('btn7').style.animation = "bottom_left_to_bottom_middle 1s linear";
+        document.getElementById('btn9').style.animation = "bottom_right_to_bottom_middle 1s linear";
+        screen_lock();
+        if (times % 2 != 0) {
+            document.getElementById('turn2').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_X, 1000);
+        }
+        else {
+            document.getElementById('turn1').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_O, 1000);
+        }
+        return 1;
+    }
+    else if ((square[0] == square[3]) && (square[3] == square[6]) && ((square[0] == 1) || (square[0] == 2))) {
+        document.getElementById('btn1').style.animation = "top_left_to_left_middle 1s linear";
+        document.getElementById('btn7').style.animation = "bottom_left_to_left_middle 1s linear";
+        screen_lock();
+        if (times % 2 != 0) {
+            document.getElementById('turn2').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_X, 1000);
+        }
+        else {
+            document.getElementById('turn1').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_O, 1000);
+        }
+        return 1;
+    }
+    else if ((square[1] == square[4]) && (square[4] == square[7]) && ((square[1] == 1) || (square[1] == 2))) {
+        document.getElementById('btn2').style.animation = "top_middle_to_middle 1s linear";
+        document.getElementById('btn8').style.animation = "bottom_middle_to_middle 1s linear";
+        screen_lock();
+        if (times % 2 != 0) {
+            document.getElementById('turn2').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_X, 1000);
+        }
+        else {
+            document.getElementById('turn1').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_O, 1000);
+        }
+        return 1;
+    }
+    else if ((square[2] == square[5]) && (square[5] == square[8]) && ((square[2] == 1) || (square[2] == 2))) {
+        document.getElementById('btn3').style.animation = "top_right_to_right_middle 1s linear";
+        document.getElementById('btn9').style.animation = "bottom_right_to_right_middle 1s linear";
+        screen_lock();
+        if (times % 2 != 0) {
+            document.getElementById('turn2').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_X, 1000);
+        }
+        else {
+            document.getElementById('turn1').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_O, 1000);
+        }
+        return 1;
+    }
+    else if ((square[0] == square[4]) && (square[4] == square[8]) && ((square[0] == 1) || (square[0] == 2))) {
+        document.getElementById('btn1').style.animation = "top_left_to_middle 1s linear";
+        document.getElementById('btn9').style.animation = "bottom_right_to_middle 1s linear";
+        screen_lock();
+        if (times % 2 != 0) {
+            document.getElementById('turn2').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_X, 1000);
+        }
+        else {
+            document.getElementById('turn1').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_O, 1000);
+        }
+        return 1;
+    }
+    else if ((square[2] == square[4]) && (square[4] == square[6]) && ((square[2] == 1) || (square[2] == 2))) {
+        document.getElementById('btn3').style.animation = "top_right_to_middle 1s linear";
+        document.getElementById('btn7').style.animation = "bottom_left_to_middle 1s linear";
+        screen_lock();
+        if (times % 2 != 0) {
+            document.getElementById('turn2').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn1').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_X, 1000);
+        }
+        else {
+            document.getElementById('turn1').style = 'border-bottom: 2px solid cyan'
+            document.getElementById('turn2').style = 'border-bottom: 2px solid rgb(151, 151, 151)';
+            setTimeout(winner_O, 1000);
+        }
+        return 1;
+    }
+}
+function winner_X() {
+    document.getElementById('tic_tac_toe').innerHTML = "";
+    document.getElementById('tic_tac_toe').innerHTML = "<img src='winner.svg'>"+"X Wins!";
+    document.getElementById('tic_tac_toe').style = "font-size:50px;color:white;";
+    document.getElementById('score').innerHTML = "";
+}
+function winner_O() {
+    document.getElementById('tic_tac_toe').innerHTML = "";
+    document.getElementById('tic_tac_toe').innerHTML = "<img src='winner.svg'>"+"O Wins!";
+    document.getElementById('tic_tac_toe').style = "font-size:50px;color:white;";
+    document.getElementById('score').innerHTML = "";
+}
+function draw() {
+    document.getElementById('tic_tac_toe').innerHTML = "";
+    document.getElementById('tic_tac_toe').innerHTML = "<img src='draw.png'>"+"Draw:) Try Again!";
+    document.getElementById('tic_tac_toe').style = "font-size:30px;color:white;";
+    document.getElementById('score').innerHTML = "";
+}
+function screen_lock() {
+    document.getElementById('btn1').disabled = true;
+    document.getElementById('btn2').disabled = true;
+    document.getElementById('btn3').disabled = true;
+    document.getElementById('btn4').disabled = true;
+    document.getElementById('btn5').disabled = true;
+    document.getElementById('btn6').disabled = true;
+    document.getElementById('btn7').disabled = true;
+    document.getElementById('btn8').disabled = true;
+    document.getElementById('btn9').disabled = true;
+}
